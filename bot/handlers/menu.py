eec922b8e9076ab974d032aec6c1d69e78fe7995
lameR <erpresents@gmail.com>
2023-07-18 21:22:41 +0300

    refactor

--- bot/handlers/menu.py
+++ bot/handlers/menu.py
@@ -8,7 +8,7 @@ from bot.keyboards import select_menu
 
 async def menu(msg: Union[types.Message, types.CallbackQuery], session: AsyncSession):
     await check_user(msg, session)
-    msg = msg.answer if type(msg) == types.Message else msg.message.edit_text
+    msg = msg.answer if isinstance(msg, types.Message) else msg.message.edit_text
     await msg(
         text='Меню',
         reply_markup=select_menu().as_markup()
